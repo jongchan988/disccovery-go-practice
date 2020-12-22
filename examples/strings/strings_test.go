17f4533444dd58a3d5136a7ac854615e5f4fac2e
jongchan988 <jongchan988@gmail.com>
2020-12-22 22:43:35 +0900

    example 디렉토리추가
    문자열 처리 관련 예제 코드 추가

---
+++ examples/strings/strings_test.go
@@ -0,0 +1,43 @@
+package strings
+
+import "fmt"
+
+func Example_printBytes() {
+	s := "가나다"
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%x:", s[i])
+		//16진수 형식으로 출력한다.
+	}
+	fmt.Println()
+	// Output:
+	// ea:b0:80:eb:82:98:eb:8b:a4:
+}
+
+func Example_printBytes2() {
+	s := "가나다"
+	fmt.Printf("%x\n", s)
+	fmt.Printf("% x\n", s)
+	// Output:
+	// eab080eb8298eb8ba4
+	// ea b0 80 eb 82 98 eb 8b a4
+}
+
+func Example_modifyBytes() {
+	b := []byte("가나다")
+	b[2]++
+	fmt.Println(string(b))
+	// Output:
+	// 각나다
+}
+
+func Example_strCat() {
+	//문자열 잇기
+	s := "abc"
+	ps := &s
+	s += "def"
+	fmt.Println(s)
+	fmt.Println(*ps)
+	// Output:
+	// abcdef
+	// abcdef
+}
