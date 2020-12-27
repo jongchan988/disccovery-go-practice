d8c788a70aceae4fb9d39e5ef438848de9ec050e
jongchan988 <jongchan988@gmail.com>
2020-12-27 17:26:26 +0900

    Example_slicing 함수 추가

---
+++ examples/slice/slice_test.go
@@ -0,0 +1,17 @@
+package slice
+
+import "fmt"
+
+func Example_slicing() {
+	nums := []int{1, 2, 3, 4, 5}
+	fmt.Println(nums)
+	fmt.Println(nums[1:3])
+	fmt.Println(nums[2:])
+	fmt.Println(nums[:3])
+
+	// Output:
+	//[1 2 3 4 5]
+	//[2 3]
+	//[3 4 5]
+	//[1 2 3]
+}
