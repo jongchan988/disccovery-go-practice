8a889256b641d7a3a8f1950f35a9a65deb346e60
jongchan988 <jongchan988@gmail.com>
2020-12-27 17:37:36 +0900

    Example_append 함수 추가

--- examples/slice/slice_test.go
+++ examples/slice/slice_test.go
@@ -15,3 +15,21 @@ func Example_slicing() {
 	//[3 4 5]
 	//[1 2 3]
 }
+
+func Example_append() {
+	f1 := []string{"사과", "바나나", "딸기"}
+	f2 := []string{"포도", "딸기"}
+	f3 := append(f1, f2...) //이어붙이기
+	f4 := append(f1[:2], f2...)
+
+	fmt.Println(f1)
+	fmt.Println(f2)
+	fmt.Println(f3)
+	fmt.Println(f4)
+
+	//Output:
+	//[사과 바나나 딸기]
+	//[포도 딸기]
+	//[사과 바나나 딸기 포도 딸기]
+	//[사과 바나나 포도 딸기]
+}
